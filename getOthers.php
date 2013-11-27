27dc51635cca468b3505b60c4e2b4dfb483e7d5e
farrellhartomo <farrell.yodihartomo@gmail.com>
2013-11-27 09:43:43 +0700

    Update getOthers.php

--- getOthers.php
+++ getOthers.php
@@ -3,7 +3,7 @@
 	$nameItem = $_GET['inputItem']; 
 	$xml=simplexml_load_file("$nameURI");
 	
-	if ($nameItem == NULL && isset($nameURI)) {
+	if (($nameItem == NULL || $nameItem == 'semua') && isset($nameURI)) {
 		echo "<table border='1'>";
 		echo "<tr>";
 		foreach($xml->children()->children() as $key)
@@ -20,27 +20,22 @@
 		}
 		echo "</tr></table>";
 	} else if (isset($nameURI) && isset($nameItem)){
-		header("content-type: text/xml");
-		echo "<".$xml->getName().">";
-		if($nameItem == 'semua'){
-			foreach ($xml->children() as $key) {
-				echo "<".$key->getName().">";
-					foreach ($key->children() as $nextkey) {
-						echo "<".$nextkey->getName().">".$nextkey."</".$nextkey->getName().">";
-					}
-				echo "</".$key->getName().">";
-			}
-		} else {
-			foreach ($xml->children() as $key) {
-				if ($key->children() == $nameItem){
-					echo "<".$key->getName().">";
-					foreach ($key->children() as $nextkey) {
-						echo "<".$nextkey->getName().">".$nextkey."</".$nextkey->getName().">";
-					}
-					echo "</".$key->getName().">";
+		echo "<table border='1'>";
+		echo "<tr>";
+		foreach($xml->children()->children() as $key)
+		{
+			echo "<td>",$key->getName(),"</td>";
+		}
+		foreach ($xml->children() as $value) {
+			if($value->children() == $nameItem){
+				echo "<tr>";
+				foreach($value->children() as $key)
+				{
+					echo "<td>",$key,"</td>";
 				}
+				echo "</tr>";
 			}
 		}
-		echo "</".$xml->getName().">";
+		echo "</tr></table>";
 	}	
  ?>
