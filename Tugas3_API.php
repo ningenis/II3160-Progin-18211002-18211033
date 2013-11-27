aadef720634a077f9a78f39fd8d7e29eb6585ebf
farrellhartomo <farrell.yodihartomo@gmail.com>
2013-11-27 09:42:37 +0700

    Update Tugas3_API.php

--- Tugas3_API.php
+++ Tugas3_API.php
@@ -7,18 +7,13 @@
 	<div class="self">
 		<form action="getSelf.php" method='get'>
 			<h4>Data Record (Data lokal) [Tugas 2]</h4>
-			<p>Pencarian berdasarkan ID: 1 - 26 </p>
-			<p>tulis 'semua' untuk mendapatkan semua data</p>
-			<input type="form" name='input'>
+			masukkan nomor ID (1 - 26): <input type="form" name='input'>
 			<input type="submit"><br>
 		</form>
 	</div>
 	<div class="other">
 		<form action="getOthers.php" method='get'>
-			<h4><b>Data URI Lain  [Tugas 3]</b></h4>
-			<p>Masukkan URI terlebih dahulu</p>
-			<p>Lihat tabel : masukkan URI, lalu kosongkan pencarian</p>
-			<p>Lakukan pencarian spesifik : masukkan URI, lalu masukkan pencarian pada kolom pertama pada tabel</p>
+			<h4><b>Data URI Lain [Tugas 3]</b></h4>
 			masukkan URI 	   : <input type="form" name='inputURI'><br>
 			masukkan Pencarian : <input type="form" name='inputItem'><br>
 			<input type="submit"><br><br>
